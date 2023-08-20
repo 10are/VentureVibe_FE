832abea36b83b4bba8e5862f6ab7aa523d3a70ce
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2023-08-20 21:59:12 +0300

    Added styles, error messages  to login, register

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,12 +4,15 @@ import { useNavigate } from 'react-router-dom';
 import '../style/LoginForm.css';
 import config from '../config';
 
-
 function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const switchToRegister = () => {
+    navigate('/register');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,9 +22,8 @@ function LoginForm() {
     })
     .then(response => {
         console.log(response.data);
-        localStorage.setItem('authToken', response.data.key); 
+        localStorage.setItem('authToken', response.data.key);
 
-       
         axios.get(`${config.BASE_URL}dj-rest-auth/user/`, {
           headers: {
             Authorization: `Token ${response.data.key}`
@@ -29,7 +31,7 @@ function LoginForm() {
         })
         .then(userResponse => {
           console.log(userResponse.data);
-          localStorage.setItem('username', userResponse.data.username); 
+          localStorage.setItem('username', userResponse.data.username);
           navigate('/');
         })
         .catch(error => {
@@ -39,6 +41,7 @@ function LoginForm() {
     })
     .catch(error => {
         console.error('There was an error!', error);
+        setError('Invalid email or password. Please try again.');
     });
   };
 
@@ -46,6 +49,7 @@ function LoginForm() {
     <div className="login-container">
       <form onSubmit={handleSubmit} className="login-form">
         <h2 className="form-title">Login</h2>
+        {error && <p className="error-message">{error}</p>}
         <div className="input-group">
           <label>Email:</label>
           <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
@@ -55,6 +59,9 @@ function LoginForm() {
           <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </div>
         <button type="submit" className="submit-btn">Login</button>
+        <button type="button" className="switch-form-btn" onClick={switchToRegister}>
+          Register
+        </button>
       </form>
     </div>
   );
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -4,14 +4,16 @@ import { useNavigate } from 'react-router-dom';
 import '../style/RegisterForm.css';
 import config from '../config';
 
-
-
 function RegisterForm() {
   const [email, setEmail] = useState('');
   const [password1, setPassword1] = useState('');
   const [password2, setPassword2] = useState('');
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
+  const switchToLogin = () => {
+    navigate('/login');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,10 +24,11 @@ function RegisterForm() {
     })
     .then(response => {
         console.log(response.data);
-        navigate('/')
+        navigate('/');
     })
     .catch(error => {
         console.error('There was an error!', error);
+        setError('Registration failed. Please check your input and try again.');
     });
   };
 
@@ -33,6 +36,7 @@ function RegisterForm() {
     <div className="register-container">
       <form onSubmit={handleSubmit} className="register-form">
         <h2 className="form-title">Register</h2>
+        {error && <p className="error-message">{error}</p>}
         <div className="input-group">
           <label>Email:</label>
           <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
@@ -46,11 +50,12 @@ function RegisterForm() {
           <input type="password" name="password2" value={password2} onChange={(e) => setPassword2(e.target.value)} required />
         </div>
         <button type="submit" className="submit-btn">Register</button>
+        <button type="button" className="switch-form-btn" onClick={switchToLogin}>
+           Login
+        </button>
       </form>
     </div>
   );
 }
-
-
 
 export default RegisterForm;
